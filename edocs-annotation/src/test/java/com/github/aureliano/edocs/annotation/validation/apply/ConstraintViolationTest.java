189b1fee2c93212cdd8680a8d5a9ff902080967a
aureliano <aureliano.franca@hotmail.com>
2016-03-29 18:44:51 -0300

    Create constraint violation assertion.

---
+++ edocs-annotation/src/test/java/com/github/aureliano/edocs/annotation/validation/apply/ConstraintViolationTest.java
@@ -0,0 +1,36 @@
+package com.github.aureliano.edocs.annotation.validation.apply;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.github.aureliano.edocs.annotation.validation.NotNull;
+
+public class ConstraintViolationTest {
+
+	@Test
+	public void testEquals() {
+		ConstraintViolation c1 = new ConstraintViolation();
+		assertFalse(c1.equals(null));
+		
+		ConstraintViolation c2 = new ConstraintViolation();
+		assertEquals(c1, c2);
+		
+		c1.withMessage("A test message.");
+		assertFalse(c1.equals(c2));
+		
+		c2.withMessage("A test message.");
+		assertTrue(c1.equals(c2));
+		
+		c1.withValidator(NotNull.class);
+		assertFalse(c1.equals(c2));
+		
+		c2.withValidator(Override.class);
+		assertFalse(c1.equals(c2));
+		
+		c2.withValidator(NotNull.class);
+		assertTrue(c1.equals(c2));
+	}
+}
