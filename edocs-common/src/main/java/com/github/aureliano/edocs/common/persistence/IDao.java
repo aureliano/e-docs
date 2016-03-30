5614c21b84853f44ef661c70400cb97df5fab49b
aureliano <aureliano.franca@hotmail.com>
2016-03-30 14:35:23 -0300

    Create DAO interface.

---
+++ edocs-common/src/main/java/com/github/aureliano/edocs/common/persistence/IDao.java
@@ -0,0 +1,19 @@
+package com.github.aureliano.edocs.common.persistence;
+
+import java.util.List;
+
+@SuppressWarnings("hiding")
+public interface IDao<IEntity> {
+
+	public abstract IEntity save(IEntity entity);
+	
+	public abstract void delete(IEntity entity);
+	
+	public abstract void delete(Integer id);
+	
+	public abstract IEntity find(Integer id);
+	
+	public abstract List<IEntity> search(IEntity entity);
+	
+	public abstract List<IEntity> search(String query);
+}
