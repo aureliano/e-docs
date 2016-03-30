fceb2e2726d2dd94bf7a54835ee2f76ff7ba8be8
aureliano <aureliano.franca@hotmail.com>
2016-03-30 13:31:11 -0300

    Create entity Document.

---
+++ edocs-domain/src/main/java/com/github/aureliano/edocs/domain/entity/Document.java
@@ -0,0 +1,102 @@
+package com.github.aureliano.edocs.domain.entity;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Document {
+
+	private Integer id;
+	private Category category;
+	private String description;
+	private Date dueDate;
+	private List<Attachment> attachments;
+	
+	public Document() {
+		this.attachments = new ArrayList<>();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public Document withId(Integer id) {
+		this.id = id;
+		return this;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public Document withCategory(Category category) {
+		this.category = category;
+		return this;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public Document withDescription(String description) {
+		this.description = description;
+		return this;
+	}
+
+	public Date getDueDate() {
+		return dueDate;
+	}
+
+	public Document withDueDate(Date dueDate) {
+		this.dueDate = dueDate;
+		return this;
+	}
+
+	public List<Attachment> getAttachments() {
+		return attachments;
+	}
+
+	public Document withAttachments(List<Attachment> attachments) {
+		this.attachments = attachments;
+		return this;
+	}
+	
+	public Document attach(Attachment attachment) {
+		this.attachments.add(attachment);
+		return this;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((category == null) ? 0 : category.hashCode());
+		result = prime * result + ((dueDate == null) ? 0 : dueDate.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Document other = (Document) obj;
+		if (category != other.category)
+			return false;
+		if (dueDate == null) {
+			if (other.dueDate != null)
+				return false;
+		} else if (!dueDate.equals(other.dueDate))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
---
+++ edocs-domain/src/test/java/com/github/aureliano/edocs/domain/entity/DocumentTest.java
@@ -0,0 +1,39 @@
+package com.github.aureliano.edocs.domain.entity;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+public class DocumentTest {
+
+	@Test
+	public void testEquals() {
+		Document d1 = new Document();
+		assertTrue(d1.equals(d1));
+		
+		Document d2 = new Document();
+		assertTrue(d1.equals(d2));
+		
+		d1.withId(127);
+		assertFalse(d1.equals(d2));
+		
+		d2.withId(127);
+		assertTrue(d1.equals(d2));
+		
+		d1.withCategory(Category.AGREEMENT);
+		assertFalse(d1.equals(d2));
+		
+		d2.withCategory(Category.AGREEMENT);
+		assertTrue(d1.equals(d2));
+		
+		Date dt = new Date();
+		d1.withDueDate(dt);
+		assertFalse(d1.equals(d2));
+		
+		d2.withDueDate(dt);
+		assertTrue(d1.equals(d2));
+	}
+}
