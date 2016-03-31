086a2d57a66ead51438bbf8cf82594d7eb30731c
aureliano <aureliano.franca@hotmail.com>
2016-03-31 16:30:02 -0300

    Create document data access object.

---
+++ edocs-domain/src/main/java/com/github/aureliano/edocs/domain/dao/DocumentDao.java
@@ -0,0 +1,127 @@
+package com.github.aureliano.edocs.domain.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.github.aureliano.edocs.common.exception.EDocsException;
+import com.github.aureliano.edocs.domain.entity.Category;
+import com.github.aureliano.edocs.domain.entity.Document;
+import com.github.aureliano.edocs.domain.helper.DataTypeHelper;
+
+public class DocumentDao extends AbstractDao<Document> {
+
+	private static final Logger logger = Logger.getLogger(DocumentDao.class.getName());
+	
+	public DocumentDao() {}
+
+	@Override
+	public Document save(Document entity) {
+		return super.saveEntity(entity);
+	}
+
+	@Override
+	public void delete(Document entity) {
+		this.delete(entity.getId());
+	}
+
+	@Override
+	public void delete(Integer id) {
+		String sql = "delete from users where id = ?";
+		super.delete(sql, id);
+	}
+
+	@Override
+	public Document find(Integer id) {
+		String sql = "select * from documents where id = ?";
+		return super.findEntity(sql, id);
+	}
+
+	@Override
+	public List<Document> search(Document entity) {
+		StringBuilder sql = new StringBuilder("select id, category, due_date from documents where");
+		
+		if (entity.getId() != null) {
+			sql.append(" id = " + entity.getId());
+			return this.search(sql.toString());
+		}
+		
+		if (entity.getCategory() != null) {
+			sql.append(" category = " + entity.getCategory());
+		}
+		
+		return this.search(sql.toString());
+	}
+
+	@Override
+	public List<Document> search(String query) {
+		return super.searchEntities(query);
+	}
+
+	@Override
+	protected PreparedStatement createPreparedStatement(Document document) {
+		String sql = this.getSaveQuery(document);
+		logger.fine("Save user SQL: " + sql);
+		
+		try {
+			PreparedStatement ps = super.connection.prepareStatement(sql, new String[] {"ID"});
+			
+			ps.setString(1, document.getCategory().toString());
+			ps.setString(2, document.getDescription());
+			ps.setDate(3, DataTypeHelper.toSqlDate(document.getDueDate()));
+			
+			if (document.getId() != null) {
+				ps.setInt(4, document.getId());
+			}
+			
+			return ps;
+		} catch (SQLException ex) {
+			logger.log(Level.SEVERE, ex.getMessage(), ex);
+			throw new EDocsException(ex);
+		}
+	}
+
+	@Override
+	protected Document fillEntity(ResultSet rs) throws SQLException {
+		List<Document> data = this.fillEntities(rs);
+		return (data.isEmpty()) ? null : data.get(0);
+	}
+
+	@Override
+	protected List<Document> fillEntities(ResultSet rs) throws SQLException {
+		List<Document> data = new ArrayList<>();
+		
+		while (rs.next()) {
+			Document e = new Document()
+				.withId(rs.getInt("id"))
+				.withCategory(Category.valueOf(rs.getString("category")))
+				.withDueDate(DataTypeHelper.toJavaDate(rs.getDate("due_date")));
+			
+			if (rs.getMetaData().getColumnCount() == 4) {
+				e.withDescription(rs.getString("description"));
+			}
+			
+			data.add(e);
+		}
+		
+		logger.fine("Found " + data.size() + " entities.");
+		return data;
+	}
+
+	@Override
+	protected Logger getLogger() {
+		return logger;
+	}
+	
+	private String getSaveQuery(Document document) {
+		if (document.getId() == null) {
+			return "insert into documents(category, description, due_date) values(?, ?, ?)";
+		} else {
+			return "update documents set category = ?, description = ?, due_date = ? where id = ?";
+		}
+	}
+}
---
+++ edocs-domain/src/test/java/com/github/aureliano/edocs/domain/dao/DocumentDaoTest.java
@@ -0,0 +1,224 @@
+package com.github.aureliano.edocs.domain.dao;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.github.aureliano.edocs.common.exception.ValidationException;
+import com.github.aureliano.edocs.common.message.ContextMessage;
+import com.github.aureliano.edocs.common.message.SeverityLevel;
+import com.github.aureliano.edocs.common.persistence.IDao;
+import com.github.aureliano.edocs.common.persistence.IPersistenceManager;
+import com.github.aureliano.edocs.common.persistence.PersistenceService;
+import com.github.aureliano.edocs.domain.entity.Attachment;
+import com.github.aureliano.edocs.domain.entity.Category;
+import com.github.aureliano.edocs.domain.entity.Document;
+import com.github.aureliano.edocs.domain.helper.PersistenceHelper;
+
+public class DocumentDaoTest {
+
+	private IDao<Document> dao;
+	
+	public DocumentDaoTest() {
+		PersistenceHelper.instance().prepareDatabase();
+		this.dao = new DocumentDao();
+	}
+
+	@Before
+	public void beforeTest() throws SQLException {
+		PersistenceHelper.instance().executeUpdate("delete from documents");
+	}
+	
+	@Test
+	public void testValidateSaveAction() {
+		this.checkInvalidCategory();
+		this.checkInvalidDescription();
+		this.checkInvalidAttachments();
+	}
+	
+	@Test
+	public void testSave() {
+		Document d = new Document()
+			.withCategory(Category.AGREEMENT)
+			.withDescription(this.getValidDescription())
+			.withAttachments(Arrays.asList(new Attachment()))
+			.withDueDate(this.getToday());
+		
+		Document document = this.dao.save(d);
+		assertNotNull(document.getId());
+		assertEquals(d.getCategory(), document.getCategory());
+		assertEquals(d.getDescription(), document.getDescription());
+		assertEquals(d.getDueDate(), document.getDueDate());
+	}
+	
+	@Test
+	public void testDeleteByEntity() throws SQLException {
+		Category category = Category.INVOICE;
+		Document d = new Document()
+			.withCategory(category)
+			.withDescription(this.getValidDescription())
+			.withAttachments(Arrays.asList(new Attachment()));
+		
+		Document document = this.dao.save(d);
+		
+		ResultSet rs = PersistenceHelper.instance().executeQuery("select count(id) from documents where category = '" + category + "'");
+		rs.next();
+		assertEquals(1, rs.getInt(1));
+		rs.close();
+		
+		this.dao.delete(document);
+		
+		rs = PersistenceHelper.instance().executeQuery("select count(id) from users where name = '" + category + "'");
+		rs.next();
+		assertEquals(0, rs.getInt(1));
+		rs.close();
+	}
+	
+	@Test
+	public void testDeleteById() throws SQLException {
+		Category category = Category.INVOICE;
+		Document d = new Document()
+			.withCategory(category)
+			.withDescription(this.getValidDescription())
+			.withAttachments(Arrays.asList(new Attachment()));
+		
+		Document document = this.dao.save(d);
+		
+		ResultSet rs = PersistenceHelper.instance().executeQuery("select count(id) from documents where category = '" + category + "'");
+		rs.next();
+		assertEquals(1, rs.getInt(1));
+		rs.close();
+		
+		this.dao.delete(document.getId());
+		
+		rs = PersistenceHelper.instance().executeQuery("select count(id) from users where name = '" + category + "'");
+		rs.next();
+		assertEquals(0, rs.getInt(1));
+		rs.close();
+	}
+	
+	@Test
+	public void testFind() {
+		Document d = new Document()
+			.withCategory(Category.INVOICE)
+			.withDescription(this.getValidDescription())
+			.withAttachments(Arrays.asList(new Attachment()));
+		
+		Document doc1 = this.dao.save(d);
+		Document doc2 = this.dao.find(doc1.getId());
+		
+		assertEquals(doc1, doc2);
+	}
+	
+	@Test
+	public void testSearchByEntity() {
+		Document d = new Document()
+			.withCategory(Category.AGREEMENT)
+			.withDescription(this.getValidDescription())
+			.withAttachments(Arrays.asList(new Attachment()));
+		
+		Document doc1 = this.dao.save(d);
+		this.dao.save(new Document()
+			.withCategory(Category.CHECK)
+			.withDescription(this.getValidDescription())
+			.withAttachments(Arrays.asList(new Attachment())));
+		
+		List<Document> data = this.dao.search(doc1);
+		
+		assertEquals(1, data.size());
+		
+		Document doc2 = data.get(0);		
+		assertEquals(doc1, doc2);
+	}
+	
+	@Test
+	public void testSearchByQuery() {
+		Document d = new Document()
+			.withCategory(Category.AGREEMENT)
+			.withDescription(this.getValidDescription())
+			.withAttachments(Arrays.asList(new Attachment()));
+		
+		Document doc1 = this.dao.save(d);
+		this.dao.save(new Document()
+			.withCategory(Category.CHECK)
+			.withDescription(this.getValidDescription())
+			.withAttachments(Arrays.asList(new Attachment())));
+		
+		List<Document> data = this.dao.search("select * from documents where category = 'AGREEMENT'");
+		
+		assertEquals(1, data.size());
+		
+		Document doc2 = data.get(0);		
+		assertEquals(doc1, doc2);
+	}
+
+	private void checkInvalidCategory() {
+		Document d = new Document()
+			.withDescription(this.getValidDescription())
+			.withAttachments(Arrays.asList(new Attachment()));
+		this.validateContextMessage(d, "Expected to find a not null value for field category.");
+	}
+
+	private void checkInvalidDescription() {
+		Document d = new Document()
+			.withCategory(Category.AGREEMENT)
+			.withAttachments(Arrays.asList(new Attachment()));
+		this.validateContextMessage(d, "Expected to find a not empty text for field description.");
+		
+		d.withDescription("1");
+		this.validateContextMessage(d, "Expected field description to have size between 5 and 1000 but got 1.");
+		
+		d.withDescription(this.getValidDescription() + "1");
+		this.validateContextMessage(d, "Expected field description to have size between 5 and 1000 but got 1001.");
+	}
+	
+	private void checkInvalidAttachments() {
+		Document d = new Document()
+			.withCategory(Category.AGREEMENT)
+			.withDescription(this.getValidDescription());
+		this.validateContextMessage(d, "Expected field hasAttachment to be true but got false.");
+	}
+
+	private String getValidDescription() {
+		StringBuilder d = new StringBuilder();
+		for (short i = 0; i < 1000; i++) {
+			d.append(".");
+		}
+		
+		return d.toString();
+	}
+	
+	private void validateContextMessage(Document document, String message) {
+		IPersistenceManager pm = PersistenceService.instance().getPersistenceManager();
+		pm.clearContextMessages();
+		
+		try {
+			this.dao.save(document);
+		} catch (ValidationException ex) {
+			assertEquals(1, pm.getContextMessages().size());
+			
+			ContextMessage m = pm.getContextMessages().iterator().next();
+			assertEquals(SeverityLevel.ERROR, m.getSeverityLevel());
+			assertEquals(message, m.getMessage());
+		}
+	}
+
+	private Date getToday() {
+		Calendar c = Calendar.getInstance();
+		c.set(Calendar.HOUR_OF_DAY, 0);
+		c.set(Calendar.MINUTE, 0);
+		c.set(Calendar.SECOND, 0);
+		c.set(Calendar.MILLISECOND, 0);
+		
+		return c.getTime();
+	}
+}
