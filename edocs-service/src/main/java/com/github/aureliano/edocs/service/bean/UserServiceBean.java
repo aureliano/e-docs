8114d9f9be1da16cd58974caa5619bfb82006c65
aureliano <aureliano.franca@hotmail.com>
2016-04-01 17:08:10 -0300

    Add user service methods signatures.

--- edocs-service/src/main/java/com/github/aureliano/edocs/service/bean/UserServiceBean.java
+++ edocs-service/src/main/java/com/github/aureliano/edocs/service/bean/UserServiceBean.java
@@ -1,5 +1,29 @@
 package com.github.aureliano.edocs.service.bean;
 
+import com.github.aureliano.edocs.common.exception.ServiceException;
+import com.github.aureliano.edocs.domain.entity.User;
+
 public class UserServiceBean implements IServiceBean {
 
+	public UserServiceBean() {}
+	
+	public User createDatabaseUser(String name, String password) {
+		throw new ServiceException("Not implemented yet.");
+	}
+	
+	public User saveUser(User user) {
+		throw new ServiceException("Not implemented yet.");
+	}
+	
+	public boolean isValidCredential(String name, String passwor) {
+		throw new ServiceException("Not implemented yet.");
+	}
+	
+	public void deleteUser(User user) {
+		throw new ServiceException("Not implemented yet.");
+	}
+	
+	public User findUserById(Integer id) {
+		throw new ServiceException("Not implemented yet.");
+	}
 }
