c45b291caea6aefaab6b53a3c1ef3631fedc33cc
aureliano <aureliano.franca@hotmail.com>
2016-04-04 17:03:22 -0300

    Implementation of findDocumentsByOwner service method.

--- edocs-service/src/main/java/com/github/aureliano/edocs/service/bean/DocumentServiceBean.java
+++ edocs-service/src/main/java/com/github/aureliano/edocs/service/bean/DocumentServiceBean.java
@@ -1,8 +1,12 @@
 package com.github.aureliano.edocs.service.bean;
 
+import java.util.List;
+
+import com.github.aureliano.edocs.common.persistence.DataPagination;
 import com.github.aureliano.edocs.common.persistence.IPersistenceManager;
 import com.github.aureliano.edocs.common.persistence.PersistenceService;
 import com.github.aureliano.edocs.domain.entity.Document;
+import com.github.aureliano.edocs.domain.entity.User;
 
 public class DocumentServiceBean implements IServiceBean {
 
@@ -14,5 +18,10 @@ public class DocumentServiceBean implements IServiceBean {
 
 	public Document findDocumentById(Integer id) {
 		return this.pm.find(Document.class, id);
+	}
+	
+	public List<Document> findDocumentsByOwner(User owner) {
+		Document document = new Document().withOwner(owner);
+		return this.pm.search(new DataPagination<Document>().withEntity(document));
 	}
 }
--- edocs-service/src/test/java/com/github/aureliano/edocs/service/bean/DocumentServiceBeanTest.java
+++ edocs-service/src/test/java/com/github/aureliano/edocs/service/bean/DocumentServiceBeanTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.sql.SQLException;
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,11 +12,14 @@ import org.junit.Test;
 import com.github.aureliano.edocs.common.config.AppConfiguration;
 import com.github.aureliano.edocs.common.config.ConfigurationSingleton;
 import com.github.aureliano.edocs.common.helper.ConfigurationHelper;
+import com.github.aureliano.edocs.common.persistence.DataPagination;
 import com.github.aureliano.edocs.common.persistence.PersistenceService;
 import com.github.aureliano.edocs.domain.dao.DocumentDao;
+import com.github.aureliano.edocs.domain.dao.UserDao;
 import com.github.aureliano.edocs.domain.entity.Attachment;
 import com.github.aureliano.edocs.domain.entity.Category;
 import com.github.aureliano.edocs.domain.entity.Document;
+import com.github.aureliano.edocs.domain.entity.User;
 import com.github.aureliano.edocs.domain.helper.PersistenceHelper;
 import com.github.aureliano.edocs.service.EdocsServicePersistenceManager;
 
@@ -55,11 +59,46 @@ public class DocumentServiceBeanTest {
 		assertEquals(document1, document2);
 	}
 	
+	@Test
+	public void testFindAttachmentsByDocument() {
+		User owner = this.createUserSample();
+		int totalDocuments = 4;
+		
+		for (byte i = 0; i < totalDocuments; i++) {
+			this.createDocumentSample(owner);
+		}
+		
+		int totalOtherDocuments = 2;
+		for (byte i = 0; i < totalOtherDocuments; i++) {
+			this.createDocumentSample();
+		}
+		
+		List<Document> res = this.bean.findDocumentsByOwner(owner);
+		assertEquals(totalDocuments, res.size());
+		
+		res = new DocumentDao().search(
+				new DataPagination<Document>().withEntity(new Document()));
+		assertEquals((totalDocuments + totalOtherDocuments), res.size());
+	}
+	
 	private Document createDocumentSample() {
+		return this.createDocumentSample(null);
+	}
+	
+	private Document createDocumentSample(User owner) {
 		Document d = new Document()
 			.withCategory(Category.AGREEMENT)
 			.withDescription("description")
+			.withOwner(owner)
 			.withAttachments(Arrays.asList(new Attachment()));
 		return new DocumentDao().save(d);
+	}
+	
+	private User createUserSample() {
+		User user = new User()
+			.withDbUser(false)
+			.withName("maria")
+			.withPassword("test123");
+		return new UserDao().save(user);
 	}
 }
