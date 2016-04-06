de76cceb32851f57446a0e3b50e54fc01db19dac
aureliano <aureliano.franca@hotmail.com>
2016-04-06 10:25:17 -0300

    Use hash generator class.

--- edocs-secure/src/main/java/com/github/aureliano/edocs/secure/hash/PasswordHashGenerator.java
+++ edocs-secure/src/main/java/com/github/aureliano/edocs/secure/hash/PasswordHashGenerator.java
@@ -1,11 +1,5 @@
 package com.github.aureliano.edocs.secure.hash;
 
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
-import javax.xml.bind.DatatypeConverter;
-
 import com.github.aureliano.edocs.common.config.AppConfiguration;
 import com.github.aureliano.edocs.common.config.ConfigurationSingleton;
 import com.github.aureliano.edocs.common.config.SecureConfiguration;
@@ -19,12 +13,8 @@ public final class PasswordHashGenerator {
 	public static final String generate(PasswordEncryptionModel pwd) {
 		String text = pwd.getPassword() + pwd.getSalt();
 		
-		try {
-			for (short i = 0; i < pwd.getHashIterations(); i++) {
-				text = hash(text, pwd.getAlgorithm().getLabel());
-			}
-		} catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
-			throw new SecurityException(ex);
+		for (short i = 0; i < pwd.getHashIterations(); i++) {
+			text = HashGenerator.generateHash(text, pwd.getAlgorithm().getLabel());
 		}
 		
 		return text;
@@ -39,15 +29,5 @@ public final class PasswordHashGenerator {
 			.withHashIterations(configuration.getHashIterations())
 			.withSalt(configuration.getSalt())
 			.withPassword(password));
-	}
-	
-	private static String hash(String text, String algorithm) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-		MessageDigest md = MessageDigest.getInstance(algorithm);
-		md.update(text.getBytes("UTF-8"));
-		
-		byte bytes[] = md.digest();
-		String hash = DatatypeConverter.printBase64Binary(bytes);
-		
-		return hash;
 	}
 }
