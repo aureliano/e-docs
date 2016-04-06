6f2260d0ea2046127482442a5deab3891515a725
aureliano <aureliano.franca@hotmail.com>
2016-04-06 16:43:22 -0300

    Add property limboPath to repository configuration.

--- edocs-common/src/main/java/com/github/aureliano/edocs/common/config/FileRepositoryConfiguration.java
+++ edocs-common/src/main/java/com/github/aureliano/edocs/common/config/FileRepositoryConfiguration.java
@@ -3,6 +3,7 @@ package com.github.aureliano.edocs.common.config;
 public class FileRepositoryConfiguration {
 
 	private String rootPath;
+	private String limboPath;
 	
 	public FileRepositoryConfiguration() {}
 	
@@ -12,6 +13,15 @@ public class FileRepositoryConfiguration {
 	
 	public FileRepositoryConfiguration withRootPath(String rootPath) {
 		this.rootPath = rootPath;
+		return this;
+	}
+
+	public String getLimboPath() {
+		return limboPath;
+	}
+
+	public FileRepositoryConfiguration withLimboPath(String limboPath) {
+		this.limboPath = limboPath;
 		return this;
 	}
 }
