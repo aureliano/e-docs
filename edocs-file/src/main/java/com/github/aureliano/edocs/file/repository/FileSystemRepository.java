2e837391e78f25b2bceb7e88f14eb5281a504bdc
aureliano <aureliano.franca@hotmail.com>
2016-04-06 14:18:08 -0300

    Create file system repository.

---
+++ edocs-file/src/main/java/com/github/aureliano/edocs/file/repository/FileSystemRepository.java
@@ -0,0 +1,58 @@
+package com.github.aureliano.edocs.file.repository;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.List;
+
+public class FileSystemRepository implements IRepository {
+
+	public FileSystemRepository() {}
+	
+	@Override
+	public Long getAvailableDiskSapce() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Long getDiskSize() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String createDir(String path) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> listFiles(String path) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public InputStream getFileStream(String path) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public File getFile(String path) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String saveFile(String path) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteFile(String path) {
+		// TODO Auto-generated method stub
+		
+	}
+}
