b82ac6c5256229efed865164f7cc8ea407b5af7c
aureliano <aureliano.franca@hotmail.com>
2016-04-07 17:03:03 -0300

    Make main frame visible by other components.

--- edocs-app/src/main/java/com/github/aureliano/edocs/app/EdocsApp.java
+++ edocs-app/src/main/java/com/github/aureliano/edocs/app/EdocsApp.java
@@ -6,15 +6,8 @@ import com.github.aureliano.edocs.app.gui.AppFrame;
 
 public class EdocsApp {
 
+	private static EdocsApp instance;
 	private AppFrame frame;
-	
-	public EdocsApp() {
-		this.frame = new AppFrame();
-	}
-	
-	public AppFrame getFrame() {
-		return this.frame;
-	}
 	
 	public static void main(String[] args) {
 		final EdocsApp application = new EdocsApp();
@@ -26,5 +19,21 @@ public class EdocsApp {
 				application.getFrame().showFrame();
 			}
 		});
+	}
+	
+	public static EdocsApp instance() {
+		if (instance == null) {
+			instance = new EdocsApp();
+		}
+		
+		return instance;
+	}
+	
+	public EdocsApp() {
+		this.frame = new AppFrame();
+	}
+	
+	public AppFrame getFrame() {
+		return this.frame;
 	}
 }
