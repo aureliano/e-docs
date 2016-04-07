de4d8c270af11247c3f7bbd0174c5e3d2a91684e
aureliano <aureliano.franca@hotmail.com>
2016-04-07 14:30:11 -0300

    Add menu items: close, close all.

--- edocs-app/src/main/java/com/github/aureliano/edocs/app/gui/menu/FileMenu.java
+++ edocs-app/src/main/java/com/github/aureliano/edocs/app/gui/menu/FileMenu.java
@@ -1,17 +1,31 @@
 package com.github.aureliano.edocs.app.gui.menu;
 
 import javax.swing.JMenu;
+import javax.swing.JSeparator;
 
 public class FileMenu extends JMenu {
 
 	private static final long serialVersionUID = -662548298147505185L;
 
+	private CloseTabMenuItem closeTabMenuItem;
+	private CloseAllTabsMenuItem closeAllTabsMenuItem;
+	private ExitMenuItem exitMenuItem;
+	
 	public FileMenu() {
 		super.setText("File");
 		this.addMenuItems();
 	}
 
 	private void addMenuItems() {
-		super.add(new ExitMenuItem());
+		this.closeTabMenuItem = new CloseTabMenuItem();
+		this.closeAllTabsMenuItem = new CloseAllTabsMenuItem();
+		this.exitMenuItem = new ExitMenuItem();
+		
+		super.add(this.closeTabMenuItem);
+		super.add(this.closeAllTabsMenuItem);
+		
+		super.add(new JSeparator());
+		
+		super.add(this.exitMenuItem);
 	}
 }
