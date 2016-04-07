ee596b26886946f023fbe0bc325a1741f1b93990
aureliano <aureliano.franca@hotmail.com>
2016-04-07 14:35:14 -0300

    Create about menu item.

---
+++ edocs-app/src/main/java/com/github/aureliano/edocs/app/gui/menu/AboutMenuItem.java
@@ -0,0 +1,12 @@
+package com.github.aureliano.edocs.app.gui.menu;
+
+import javax.swing.JMenuItem;
+
+public class AboutMenuItem extends JMenuItem {
+
+	private static final long serialVersionUID = 3258389699083751538L;
+
+	public AboutMenuItem() {
+		super.setText("About");
+	}
+}
