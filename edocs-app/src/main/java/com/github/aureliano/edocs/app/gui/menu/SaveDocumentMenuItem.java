4ff7fde1ae21143d46ff468fb7a0dcb530a358b4
aureliano <aureliano.franca@hotmail.com>
2016-04-07 14:08:43 -0300

    Create save document menu item.

---
+++ edocs-app/src/main/java/com/github/aureliano/edocs/app/gui/menu/SaveDocumentMenuItem.java
@@ -0,0 +1,12 @@
+package com.github.aureliano.edocs.app.gui.menu;
+
+import javax.swing.JMenuItem;
+
+public class SaveDocumentMenuItem extends JMenuItem {
+
+	private static final long serialVersionUID = -2349844888548109933L;
+
+	public SaveDocumentMenuItem() {
+		super.setText("Save");
+	}
+}
