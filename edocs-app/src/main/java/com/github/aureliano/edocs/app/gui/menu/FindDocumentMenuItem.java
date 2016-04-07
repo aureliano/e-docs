69447a1648288e631e6dc369d74274fd1ca0ddf5
aureliano <aureliano.franca@hotmail.com>
2016-04-07 14:08:57 -0300

    Create find document menu item.

---
+++ edocs-app/src/main/java/com/github/aureliano/edocs/app/gui/menu/FindDocumentMenuItem.java
@@ -0,0 +1,12 @@
+package com.github.aureliano.edocs.app.gui.menu;
+
+import javax.swing.JMenuItem;
+
+public class FindDocumentMenuItem extends JMenuItem {
+
+	private static final long serialVersionUID = -2048071523465789664L;
+
+	public FindDocumentMenuItem() {
+		super.setText("Find document...");
+	}
+}
