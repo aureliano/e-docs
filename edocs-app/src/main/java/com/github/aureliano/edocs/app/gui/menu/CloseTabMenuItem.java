5effb077483ea89aef206bff9a31de28a46739bf
aureliano <aureliano.franca@hotmail.com>
2016-04-07 14:29:28 -0300

    Create close tab menu item.

---
+++ edocs-app/src/main/java/com/github/aureliano/edocs/app/gui/menu/CloseTabMenuItem.java
@@ -0,0 +1,12 @@
+package com.github.aureliano.edocs.app.gui.menu;
+
+import javax.swing.JMenuItem;
+
+public class CloseTabMenuItem extends JMenuItem {
+
+	private static final long serialVersionUID = -3834903211976535165L;
+
+	public CloseTabMenuItem() {
+		super.setText("Close");
+	}
+}
