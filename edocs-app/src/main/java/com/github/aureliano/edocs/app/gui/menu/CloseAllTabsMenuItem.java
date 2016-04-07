3026c2e584f3cb1feb265a37a98083ca56984e20
aureliano <aureliano.franca@hotmail.com>
2016-04-07 14:29:52 -0300

    Create close all tabs menu item.

---
+++ edocs-app/src/main/java/com/github/aureliano/edocs/app/gui/menu/CloseAllTabsMenuItem.java
@@ -0,0 +1,12 @@
+package com.github.aureliano.edocs.app.gui.menu;
+
+import javax.swing.JMenuItem;
+
+public class CloseAllTabsMenuItem extends JMenuItem {
+
+	private static final long serialVersionUID = 5402503871207802801L;
+
+	public CloseAllTabsMenuItem() {
+		super.setText("Close all");
+	}
+}
