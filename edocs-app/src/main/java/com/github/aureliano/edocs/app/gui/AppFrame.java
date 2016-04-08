fa5fb9d1af5bea2808d27e35f7676aedd3038adf
aureliano <aureliano.franca@hotmail.com>
2016-04-08 13:33:32 -0300

    Apply internationalization to main frame.

--- edocs-app/src/main/java/com/github/aureliano/edocs/app/gui/AppFrame.java
+++ edocs-app/src/main/java/com/github/aureliano/edocs/app/gui/AppFrame.java
@@ -7,19 +7,22 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import com.github.aureliano.edocs.app.gui.menu.MenuBar;
+import com.github.aureliano.edocs.common.locale.EdocsLocale;
 
 public class AppFrame extends JFrame {
 
 	private static final long serialVersionUID = 7618501026967569839L;
 
+	private EdocsLocale locale;
 	private TabbedPane tabbedPane;
 	
 	public AppFrame() {
+		this.locale = EdocsLocale.instance();
 		this.buildGui();
 	}
 	
 	private void buildGui() {
-		super.setTitle("e-Docs");
+		super.setTitle(this.locale.getMessage("gui.frame.main.title"));
 		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel panel = new JPanel(new BorderLayout());
