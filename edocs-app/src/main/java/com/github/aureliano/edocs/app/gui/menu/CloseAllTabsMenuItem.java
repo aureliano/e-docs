51bf01e63cd3ccd86f2aecc81f99d296276ba307
aureliano <aureliano.franca@hotmail.com>
2016-04-08 17:57:00 -0300

    Close all tabs.

--- edocs-app/src/main/java/com/github/aureliano/edocs/app/gui/menu/CloseAllTabsMenuItem.java
+++ edocs-app/src/main/java/com/github/aureliano/edocs/app/gui/menu/CloseAllTabsMenuItem.java
@@ -1,7 +1,11 @@
 package com.github.aureliano.edocs.app.gui.menu;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JMenuItem;
 
+import com.github.aureliano.edocs.app.cmd.CloseAllTabsCommand;
 import com.github.aureliano.edocs.common.locale.EdocsLocale;
 
 public class CloseAllTabsMenuItem extends JMenuItem {
@@ -10,5 +14,13 @@ public class CloseAllTabsMenuItem extends JMenuItem {
 
 	public CloseAllTabsMenuItem() {
 		super.setText(EdocsLocale.instance().getMessage("gui.menubar.file.close.all"));
+		
+		super.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new CloseAllTabsCommand().execute();
+			}
+		});
 	}
 }
