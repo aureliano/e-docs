3fecc6005679f97e9a58a9acbd829e87b6e55443
aureliano <aureliano.franca@hotmail.com>
2016-04-08 17:26:09 -0300

    Close active tab.

--- edocs-app/src/main/java/com/github/aureliano/edocs/app/gui/menu/CloseTabMenuItem.java
+++ edocs-app/src/main/java/com/github/aureliano/edocs/app/gui/menu/CloseTabMenuItem.java
@@ -1,7 +1,11 @@
 package com.github.aureliano.edocs.app.gui.menu;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JMenuItem;
 
+import com.github.aureliano.edocs.app.cmd.CloseActiveTabCommand;
 import com.github.aureliano.edocs.common.locale.EdocsLocale;
 
 public class CloseTabMenuItem extends JMenuItem {
@@ -10,5 +14,13 @@ public class CloseTabMenuItem extends JMenuItem {
 
 	public CloseTabMenuItem() {
 		super.setText(EdocsLocale.instance().getMessage("gui.menubar.file.close"));
+		
+		super.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new CloseActiveTabCommand().execute();
+			}
+		});
 	}
 }
