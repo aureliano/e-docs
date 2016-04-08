b42049a041620f7ec36270fd5bfe405ec8959ec0
aureliano <aureliano.franca@hotmail.com>
2016-04-08 15:13:16 -0300

    Create new document command.

---
+++ edocs-app/src/main/java/com/github/aureliano/edocs/app/cmd/NewDocumentCommand.java
@@ -0,0 +1,26 @@
+package com.github.aureliano.edocs.app.cmd;
+
+import com.github.aureliano.edocs.app.EdocsApp;
+import com.github.aureliano.edocs.app.gui.AppFrame;
+import com.github.aureliano.edocs.app.gui.panel.SaveDocumentPanel;
+import com.github.aureliano.edocs.common.locale.EdocsLocale;
+
+public class NewDocumentCommand implements ICommand {
+
+	@Override
+	public void execute() {
+		if (!this.canExecute()) {
+			return;
+		}
+		
+		AppFrame frame = EdocsApp.instance().getFrame();
+		String title = EdocsLocale.instance().getMessage("gui.frame.main.tabbed.pane.document.new");
+		
+		frame.addTabPanel(title, new SaveDocumentPanel());
+	}
+
+	@Override
+	public boolean canExecute() {
+		return true;
+	}
+}
