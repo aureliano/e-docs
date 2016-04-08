da7eab560004c84682c4a16c1ef27528c2cd4c58
aureliano <aureliano.franca@hotmail.com>
2016-04-08 16:42:42 -0300

    Create save document panel.

--- edocs-app/src/main/java/com/github/aureliano/edocs/app/gui/panel/SaveDocumentPanel.java
+++ edocs-app/src/main/java/com/github/aureliano/edocs/app/gui/panel/SaveDocumentPanel.java
@@ -2,7 +2,11 @@ package com.github.aureliano.edocs.app.gui.panel;
 
 import java.awt.GridLayout;
 
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 import com.github.aureliano.edocs.common.locale.EdocsLocale;
 
@@ -12,12 +16,42 @@ public class SaveDocumentPanel extends JPanel {
 
 	private EdocsLocale locale;
 	
+	private JTextField textFieldName;
+	private JComboBox<Object> comboBoxCategory;
+	private JTextArea textAreaDescription;
+	private JTextField textFieldDueDate;
+	private JComboBox<Object> comboBoxOwner;
+	
 	public SaveDocumentPanel() {
 		this.locale = EdocsLocale.instance();
 		this.buildGui();
 	}
 
 	private void buildGui() {
-		super.setLayout(new GridLayout(3, 2));
+		this.initializeGui();
+		super.setLayout(new GridLayout(0, 2));
+		
+		super.add(new JLabel("Name"));
+		super.add(this.textFieldName);
+		
+		super.add(new JLabel("Category"));
+		super.add(this.comboBoxCategory);
+		
+		super.add(new JLabel("Description"));
+		super.add(this.textAreaDescription);
+		
+		super.add(new JLabel("Due date"));
+		super.add(this.textFieldDueDate);
+		
+		super.add(new JLabel("Owner"));
+		super.add(this.comboBoxOwner);
+	}
+	
+	private void initializeGui() {
+		this.textFieldName = new JTextField();
+		this.comboBoxCategory = new JComboBox<>();
+		this.textAreaDescription = new JTextArea();
+		this.textFieldDueDate = new JTextField();
+		this.comboBoxOwner = new JComboBox<>();
 	}
 }
